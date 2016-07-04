814586b09f943a177f320b8e05713a4e723b077d
caodaoxi <caodaoxi1234@163.com>
2016-07-04 16:05:41 +0800

    dsdsds

--- src/main/java/com/stock/crawler/utils/MailUtils.java
+++ src/main/java/com/stock/crawler/utils/MailUtils.java
@@ -1,50 +1,50 @@
-//package com.stock.crawler.utils;
-//
-//import com.stock.crawler.config.Configuration;
-//
-//import javax.mail.Message;
-//import javax.mail.MessagingException;
-//import javax.mail.Session;
-//import javax.mail.Transport;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
-//import java.util.Calendar;
-//import java.util.Properties;
-//
-///**
-// * Created by caodaoxi on 2015/12/17.
-// */
-//public class MailUtils {
-//    public static void sendMessage(String to, String subject, String messageText) throws MessagingException {
-//        // 第一步：配置javax.mail.Session对象
-//        Properties props = new Properties();
-//        props.put("mail.smtp.host", Configuration.SMTPHOST);
-//        props.put("mail.smtp.starttls.enable","true");//使用 STARTTLS安全连接
-//        props.put("mail.smtp.port", Configuration.MAILPORT);             //google使用465或587端口
-//        props.put("mail.smtp.auth", "true");        // 使用验证
-//        //props.put("mail.debug", "true");
-//        Session mailSession = Session.getInstance(props,new MyAuthenticator(Configuration.MAILSENDERUSERNAME,Configuration.MAILSENDERPASSWORD));
-//
-//        // 第二步：编写消息
-//        InternetAddress fromAddress = new InternetAddress(Configuration.MAILSENDERUSERNAME);
-//        InternetAddress toAddress = new InternetAddress(to);
-//
-//        MimeMessage message = new MimeMessage(mailSession);
-//
-//        message.setFrom(fromAddress);
-//        message.addRecipient(Message.RecipientType.TO, toAddress);
-//
-//        message.setSentDate(Calendar.getInstance().getTime());
-//        message.setSubject(subject);
-//        message.setContent(messageText, "text/html;charset=gb2312");
-//
-//        // 第三步：发送消息
-//        Transport transport = mailSession.getTransport("smtp");
-//        transport.connect(Configuration.SMTPHOST, Configuration.JDBCUSERNAME, Configuration.MAILPASSWORD);
-//        transport.send(message, message.getRecipients(Message.RecipientType.TO));
-//    }
-//
-//    public static void main(String[] args) throws MessagingException {
-//        MailUtils.sendMessage("cdx@gbdex.com", "股票", "开始卖了");
-//    }
-//}
+package com.stock.crawler.utils;
+
+import com.stock.crawler.config.Configuration;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Calendar;
+import java.util.Properties;
+
+/**
+ * Created by caodaoxi on 2015/12/17.
+ */
+public class MailUtils {
+    public static void sendMessage(String to, String subject, String messageText) throws MessagingException {
+        Properties props = new Properties();
+        // 开启debug调试
+        props.setProperty("mail.debug", "true");
+        // 发送服务器需要身份验证
+        props.setProperty("mail.smtp.auth", "true");
+        // 设置邮件服务器主机名
+        props.setProperty("mail.host", "mail.jzsec.com");
+        // 发送邮件协议名称
+        props.setProperty("mail.transport.protocol", "smtp");
+
+        // 设置环境信息
+        Session session = Session.getInstance(props);
+
+        // 创建邮件对象
+        Message msg = new MimeMessage(session);
+        msg.setSubject(subject);
+        // 设置邮件内容
+        msg.setText(messageText);
+        // 设置发件人
+        msg.setFrom(new InternetAddress("caodaoxi@jzsec.com"));
+
+        Transport transport = session.getTransport();
+        // 连接邮件服务器
+        transport.connect("caodaoxi@jzsec.com", "Abcd1234");
+        // 发送邮件
+        transport.sendMessage(msg, new Address[] {new InternetAddress(to)});
+        // 关闭连接
+        transport.close();
+    }
+
+    public static void main(String[] args) throws MessagingException {
+//        MailUtils.sendMessage("caodaoxi@jzsec.com", "jkkk", "jksfds");
+        System.out.println(Math.pow(1.02, 200));
+    }
+}
